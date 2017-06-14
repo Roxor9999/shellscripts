94abc06de581d60a7d70d2317b121a5649b2d9b6
Christoffer Lundell <christofferlundell2010@gmail.com>
2017-06-14 21:47:41 +0200

    Update streamchecker.sh

--- streamchecker.sh
+++ streamchecker.sh
@@ -2,7 +2,7 @@
 
 curl -H 'Accept: application/vnd.twitchtv.v5+json' \
 	-H 'Client-ID: ReplaceMe' \
-	-H 'Cookie: ReplaceMe' \
+	-H 'Cookie: persistent=ReplaceMe;' \
 	-X GET 'https://api.twitch.tv/kraken/streams/followed' \
 	| grep -o '"display_name":[^,]*,\|"viewers":[^,]*,' \
 	| paste -d ' ' - -
