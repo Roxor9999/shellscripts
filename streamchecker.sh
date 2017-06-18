c62861a88ea257f409ea558dc19933b4dd1661fa
Christoffer Lundell <christofferlundell2010@gmail.com>
2017-06-18 04:15:08 +0200

    Better formatting
    Removing the comma allows for using tab without malforming the output for 10000+ viewer streams

--- streamchecker.sh
+++ streamchecker.sh
@@ -4,5 +4,5 @@ curl -sH 'Accept: application/vnd.twitchtv.v5+json' \
 	-H 'Client-ID: ReplaceMe' \
 	-H 'Cookie: persistent=ReplaceMe;' \
 	-X GET 'https://api.twitch.tv/kraken/streams/followed' \
-	| grep -Eo '"display_name":[^,]*,|"viewers":[^,]*,' \
-	| paste -d ' ' - -
+	| grep -Eo '"display_name":[^,]*|"viewers":[^,]*' \
+	| paste -d '\t' - -
