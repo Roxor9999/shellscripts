b003994d729dc856ddf96acfe8211cd403bec8d8
Christoffer Lundell <christofferlundell2010@gmail.com>
2017-06-20 16:40:22 +0200

    Change to using jq for simplicity

--- overrustlestreamchecker.sh
+++ overrustlestreamchecker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-curl -sX GET 'https://api.overrustle.com/api/' \
-	| grep -Eo '"streams":{[^}]*' \
-	| grep -Eo '"\/[^:]*:[^,]{2,}' \
-	| sort -t : -rnk2
+curl -sX GET 'https://api.overrustle.com/api' \
+	| jq -r '.streams | to_entries | sort_by(.value) | reverse[] | select(.value>9) | "\(.value)" + "\t" + .key'
