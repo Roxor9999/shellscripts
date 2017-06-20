8a4f3d622b7ffe5fbe4cf1d80bc637fa910ee232
Roxor9999 <roxor9999@hotmail.com>
2017-06-20 18:06:13 +0200

    Update overrustlestreamchecker.sh

--- overrustlestreamchecker.sh
+++ overrustlestreamchecker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -sX GET 'https://api.overrustle.com/api' \
-	| jq -r '.streams | to_entries | sort_by(.value) | reverse[] | select(.value>9) | "\(.value)" + "\t" + .key'
+	| jq -r '.streams | to_entries | sort_by(.value*-1)[] | select(.value>9) | "\(.value)" + "\t" + .key'
